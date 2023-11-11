2fdc0475386b6b626ef0e4a938d1f353bfaa29b6
Franco Salama <salamafranco@gmail.com>
2023-11-11 19:57:51 -0300

    axios clima

--- servicio/eventos.js
+++ servicio/eventos.js
@@ -1,5 +1,6 @@
 import ModelMongoDBEvento from "../model/DAO/eventosMongoDB.js";
-import ModelMongoDBUsuario from '../model/DAO/usuariosMongoDB.js'
+import ModelMongoDBUsuario from "../model/DAO/usuariosMongoDB.js";
+import axios from "axios";
 
 class Servicio {
   constructor() {
@@ -23,23 +24,16 @@ class Servicio {
   };
 
   obtenerClima = async (id) => {
+    const evento = await this.modelEvento.obtenerEvento(id);
+
+    const ciudad = evento.ciudad;
+    const apiKey = "0194ea3482e0e589fe92a10f79156f66";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=sp,es&appid=${apiKey}&units=metric`;
+
     try {
-      const evento = await this.modelEvento.obtenerEvento(id);
-
-      const ciudad = evento.ciudad;
-      const apiKey = "0194ea3482e0e589fe92a10f79156f66";
-
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=sp,es&appid=${apiKey}&units=metric`;
-
-      const respuesta = await fetch(url);
-
-      if (!respuesta.ok) {
-        throw new Error(
-          `Error al obtener el clima. Código de estado: ${respuesta.status}`
-        );
-      }
-
-      const datosClima = await respuesta.json();
+      const respuesta = await axios.get(url);
+      const datosClima = await respuesta.data;
 
       const temperatura = datosClima.main.temp;
       const descripcion = datosClima.weather[0].description;
@@ -51,19 +45,26 @@ class Servicio {
 
       return evento;
     } catch (error) {
-      console.error("Error en obtenerClima:", error.message);
-      throw error;
+      throw new Error(
+        `Error al obtener el clima. Código de estado: ${respuesta.status}`
+      );
     }
   };
 
   crearEvento = async (evento) => {
     const eventoGuardado = await this.modelEvento.crearEvento(evento);
-    await this.modelUsuario.guardarEventoCreado(eventoGuardado.idUsuarioCreador, eventoGuardado._id)
+    await this.modelUsuario.guardarEventoCreado(
+      eventoGuardado.idUsuarioCreador,
+      eventoGuardado._id
+    );
     return eventoGuardado;
   };
 
   actualizarEvento = async (id, evento) => {
-    const eventoActualizado = await this.modelEvento.actualizarEvento(id, evento);
+    const eventoActualizado = await this.modelEvento.actualizarEvento(
+      id,
+      evento
+    );
     return eventoActualizado;
   };
 
@@ -74,7 +75,7 @@ class Servicio {
 
   suscribirUsuario = async (id, idUsuario) => {
     const evento = await this.modelEvento.suscribirUsuario(id, idUsuario);
-    await this.modelUsuario.guardarEventoSuscripto(idUsuario, id)
+    await this.modelUsuario.guardarEventoSuscripto(idUsuario, id);
     return evento;
   };
 }
